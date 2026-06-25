33d29570dfa2e0318f23c782b2506cbd5b91ee43
Meo-ICAR <piergiuseppe.meo@icar.cnr.it>
2026-06-25 21:49:27 +0200

    pull 2

--- database/seeders/OamCodeSeeder.php
+++ database/seeders/OamCodeSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder,
+use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
 class OamCodeSeeder extends Seeder
@@ -13,37 +13,38 @@ class OamCodeSeeder extends Seeder
     public function run(): void
     {
         $oamCodes = [
-            ['id' => 1, 'code' => 'A.1', 'name' => 'Mutui', 'description' => 'A.1 Mutui', 'tipo_prodotto' => 'Mutuo', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 2, 'code' => 'A.2', 'name' => 'Cessioni del V dello stipendio/pensione e delegazioni di pagamento', 'description' => 'A.2 Cessioni del V dello stipendio/pensione e delegazioni di pagamento', 'tipo_prodotto' => 'Cessione', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 3, 'code' => 'A.3', 'name' => 'Factoring crediti', 'description' => 'A.3 Factoring crediti', 'tipo_prodotto' => 'Factoring', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 4, 'code' => 'A.4', 'name' => 'Acquisto di crediti', 'description' => 'A.4 Acquisto di crediti', 'tipo_prodotto' => 'Crediti', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 5, 'code' => 'A.5', 'name' => 'Leasing autoveicoli e aeronavali', 'description' => 'A.5 Leasing autoveicoli e aeronavali', 'tipo_prodotto' => 'Autoveicoli', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 6, 'code' => 'A.6', 'name' => 'Leasing immobiliare', 'description' => 'A.6 Leasing immobiliare', 'tipo_prodotto' => 'Immobiliare', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 7, 'code' => 'A.7', 'name' => 'Leasing strumentale', 'description' => 'A.7 Leasing strumentale', 'tipo_prodotto' => 'Strumentale', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 8, 'code' => 'A.8', 'name' => 'Leasing su fonti rinnovabili ed altre tipologie di investimento', 'description' => 'A.8 Leasing su fonti rinnovabili ed altre tipologie di investimento', 'tipo_prodotto' => 'Rinnovabili', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 9, 'code' => 'A.9', 'name' => 'Aperture di credito in conto corrente', 'description' => 'A.9 Aperture di credito in conto corrente', 'tipo_prodotto' => 'Fido', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 10, 'code' => 'A.10', 'name' => 'Credito personale', 'description' => 'A.10 Credito personale', 'tipo_prodotto' => 'Prestito', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 11, 'code' => 'A.11', 'name' => 'Credito finalizzato', 'description' => 'A.11 Credito finalizzato', 'tipo_prodotto' => 'Finalizzato', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 12, 'code' => 'A.12', 'name' => 'Prestito su pegno', 'description' => 'A.12 Prestito su pegno', 'tipo_prodotto' => 'Pegno', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 13, 'code' => 'A.13', 'name' => 'Rilascio di fidejussioni e garanzie', 'description' => 'A.13 Rilascio di fidejussioni e garanzie', 'tipo_prodotto' => 'Fidejussioni', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 14, 'code' => 'A.13-bis', 'name' => 'Garanzia collettiva dei fidi', 'description' => 'A.13-bis Garanzia collettiva dei fidi', 'tipo_prodotto' => 'Fidi', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 15, 'code' => 'A.14', 'name' => 'Anticipi e sconti commerciali', 'description' => 'A.14 Anticipi e sconti commerciali', 'tipo_prodotto' => 'Anticipi', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 16, 'code' => 'A.15', 'name' => 'Credito revolving', 'description' => 'A.15 Credito revolving', 'tipo_prodotto' => 'Revolving', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 17, 'code' => 'A.16', 'name' => 'Ristrutturazione dei crediti (art. 128-quater decies, del TUB)', 'description' => 'A.16 Ristrutturazione dei crediti (art. 128-quater decies, del TUB)', 'tipo_prodotto' => 'Ristrutturazione', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 18, 'code' => 'Consulenza', 'name' => ' ', 'description' => 'Consulenza  ', 'tipo_prodotto' => 'Consulenza', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 19, 'code' => 'Segnalazione mutuo', 'name' => ' ', 'description' => 'Segnalazione mutuo  ', 'tipo_prodotto' => 'Segnalazione', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-              ['id' => 21, 'code' => 'A.4bis', 'name' => 'TFS', 'description' => 'A.4bis TFS', 'tipo_prodotto' => 'TFS', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-                   ['id' => 22, 'code' => 'A.11', 'name' => 'Credito finalizzato', 'description' => 'A.11 Credito finalizzato', 'tipo_prodotto' => 'Finalizzato', 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 23, 'code' => 'A.2', 'name' => 'Cessioni del V dello stipendio/pensione e delegazioni di pagamento', 'description' => 'A.2 Cessioni del V dello stipendio/pensione e delegazioni di pagamento', 'tipo_prodotto' => 'Delega', 'is_dummy' => true,'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-
-            ['id' => 24, 'code' => 'A.1', 'name' => 'Mutui', 'description' => 'A.1 Mutui', 'tipo_prodotto' => 'Aziendale', 'is_dummy' => true, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-
-            ['id' => 25, 'code' => 'A.1', 'name' => 'Mutui', 'description' => 'A.1 Mutui', 'tipo_prodotto' => 'Chirografario', 'is_dummy' => true,'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
-            ['id' => 26, 'code' => 'A.1', 'name' => 'Mutui', 'description' => 'A.1 Mutui', 'tipo_prodotto' => 'Microcredito', 'is_dummy' => true, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 1, 'code' => 'A.1', 'name' => 'Mutui', 'description' => 'A.1 Mutui', 'tipo_prodotto' => 'Mutuo', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 2, 'code' => 'A.2', 'name' => 'Cessioni del V dello stipendio/pensione e delegazioni di pagamento', 'description' => 'A.2 Cessioni del V dello stipendio/pensione e delegazioni di pagamento', 'tipo_prodotto' => 'Cessione', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 3, 'code' => 'A.3', 'name' => 'Factoring crediti', 'description' => 'A.3 Factoring crediti', 'tipo_prodotto' => 'Factoring', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 4, 'code' => 'A.4', 'name' => 'Acquisto di crediti', 'description' => 'A.4 Acquisto di crediti', 'tipo_prodotto' => 'Crediti', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 5, 'code' => 'A.5', 'name' => 'Leasing autoveicoli e aeronavali', 'description' => 'A.5 Leasing autoveicoli e aeronavali', 'tipo_prodotto' => 'Autoveicoli', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 6, 'code' => 'A.6', 'name' => 'Leasing immobiliare', 'description' => 'A.6 Leasing immobiliare', 'tipo_prodotto' => 'Immobiliare', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 7, 'code' => 'A.7', 'name' => 'Leasing strumentale', 'description' => 'A.7 Leasing strumentale', 'tipo_prodotto' => 'Strumentale', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 8, 'code' => 'A.8', 'name' => 'Leasing su fonti rinnovabili ed altre tipologie di investimento', 'description' => 'A.8 Leasing su fonti rinnovabili ed altre tipologie di investimento', 'tipo_prodotto' => 'Rinnovabili', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 9, 'code' => 'A.9', 'name' => 'Aperture di credito in conto corrente', 'description' => 'A.9 Aperture di credito in conto corrente', 'tipo_prodotto' => 'Fido', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 10, 'code' => 'A.10', 'name' => 'Credito personale', 'description' => 'A.10 Credito personale', 'tipo_prodotto' => 'Prestito', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 11, 'code' => 'A.11', 'name' => 'Credito finalizzato', 'description' => 'A.11 Credito finalizzato', 'tipo_prodotto' => 'Finalizzato', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 12, 'code' => 'A.12', 'name' => 'Prestito su pegno', 'description' => 'A.12 Prestito su pegno', 'tipo_prodotto' => 'Pegno', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 13, 'code' => 'A.13', 'name' => 'Rilascio di fidejussioni e garanzie', 'description' => 'A.13 Rilascio di fidejussioni e garanzie', 'tipo_prodotto' => 'Fidejussioni', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 14, 'code' => 'A.13-bis', 'name' => 'Garanzia collettiva dei fidi', 'description' => 'A.13-bis Garanzia collettiva dei fidi', 'tipo_prodotto' => 'Fidi', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 15, 'code' => 'A.14', 'name' => 'Anticipi e sconti commerciali', 'description' => 'A.14 Anticipi e sconti commerciali', 'tipo_prodotto' => 'Anticipi', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 16, 'code' => 'A.15', 'name' => 'Credito revolving', 'description' => 'A.15 Credito revolving', 'tipo_prodotto' => 'Revolving', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 17, 'code' => 'A.16', 'name' => 'Ristrutturazione dei crediti (art. 128-quater decies, del TUB)', 'description' => 'A.16 Ristrutturazione dei crediti (art. 128-quater decies, del TUB)', 'tipo_prodotto' => 'Ristrutturazione', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 18, 'code' => 'Consulenza', 'name' => ' ', 'description' => 'Consulenza  ', 'tipo_prodotto' => 'Consulenza', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 19, 'code' => 'Segnalazione mutuo', 'name' => ' ', 'description' => 'Segnalazione mutuo  ', 'tipo_prodotto' => 'Segnalazione', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 20, 'code' => 'A.4bis', 'name' => 'TFS', 'description' => 'A.4bis TFS', 'tipo_prodotto' => 'TFS', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 21, 'code' => 'A.11', 'name' => 'Credito finalizzato', 'description' => 'A.11 Credito finalizzato', 'tipo_prodotto' => 'Finalizzato', 'is_dummy' => false, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 22, 'code' => 'A.2', 'name' => 'Cessioni del V dello stipendio/pensione e delegazioni di pagamento', 'description' => 'A.2 Cessioni del V dello stipendio/pensione e delegazioni di pagamento', 'tipo_prodotto' => 'Delega', 'is_dummy' => true, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 23, 'code' => 'A.1', 'name' => 'Mutui', 'description' => 'A.1 Mutui', 'tipo_prodotto' => 'Aziendale', 'is_dummy' => true, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 24, 'code' => 'A.1', 'name' => 'Mutui', 'description' => 'A.1 Mutui', 'tipo_prodotto' => 'Chirografario', 'is_dummy' => true, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
+            ['id' => 25, 'code' => 'A.1', 'name' => 'Mutui', 'description' => 'A.1 Mutui', 'tipo_prodotto' => 'Microcredito', 'is_dummy' => true, 'created_at' => '2026-03-16 04:20:44', 'updated_at' => '2026-03-16 04:20:44'],
         ];
 
-        // Se la tabella potrebbe contenere già questi ID, 'upsert' previene errori di chiave duplicata.
-        // In alternativa, se si tratta di un'installazione pulita, 'insert' va benissimo.
-        DB::table('oam_codes')->upsert($oamCodes, ['id'], ['code', 'name', 'description', 'tipo_prodotto', 'updated_at']);
+        // L'upsert ora funzionerà correttamente perché la struttura dell'array è uniforme.
+        DB::table('oam_codes')->upsert(
+            $oamCodes,
+            ['id'],
+            ['code', 'name', 'description', 'tipo_prodotto', 'is_dummy', 'updated_at']
+        );
     }
 }
--- database/seeders/SuspiciousActivityReportSeeder.php
+++ database/seeders/SuspiciousActivityReportSeeder.php
@@ -11,7 +11,7 @@ class SuspiciousActivityReportSeeder extends Seeder
     {
         $companyId = '3ff53405-4abb-4468-bff5-f9493badac5b';
 
-        // reporter_type uses the BPM model FQN
+        // reportable_type uses the BPM model FQN
         $agentType = 'App\Models\PROFORMA\Fornitore';
         $employeeType = 'App\Models\PROFORMA\Employee';
 
@@ -19,8 +19,8 @@ class SuspiciousActivityReportSeeder extends Seeder
             [
                 'company_id' => $companyId,
                 'client_id' => 10,
-                'reporter_type' => $agentType,
-                'reporter_id' => 1,
+                'reportable_type' => $agentType,
+                'reportable_id' => 1,
                 'anomalies_codes' => json_encode(['1A', '4A']),
                 'description' => 'Il cliente ha effettuato tre operazioni di versamento in contanti in tre giorni consecutivi, ciascuna di importo appena inferiore alla soglia di €10.000. Profilo economico non coerente con le operazioni.',
                 'status' => 'reported',
@@ -29,8 +29,8 @@ class SuspiciousActivityReportSeeder extends Seeder
             [
                 'company_id' => $companyId,
                 'client_id' => 11,
-                'reporter_type' => $agentType,
-                'reporter_id' => 2,
+                'reportable_type' => $agentType,
+                'reportable_id' => 2,
                 'anomalies_codes' => json_encode(['2B', '3A']),
                 'description' => 'Operazione di trasferimento fondi verso conto estero in paese a rischio senza giustificazione economica plausibile. Il cliente ha mostrato reticenza nel fornire documentazione di supporto.',
                 'status' => 'investigated',
@@ -39,8 +39,8 @@ class SuspiciousActivityReportSeeder extends Seeder
             [
                 'company_id' => $companyId,
                 'client_id' => 12,
-                'reporter_type' => $employeeType,
-                'reporter_id' => 3,
+                'reportable_type' => $employeeType,
+                'reportable_id' => 3,
                 'anomalies_codes' => json_encode(['5B']),
                 'description' => 'Rilevata anomalia nella frequenza di operazioni in contanti: 8 prelievi in 10 giorni per importi variabili tra €2.000 e €4.500. Comportamento inusuale rispetto alla storia operativa del cliente.',
                 'status' => 'pending',
@@ -49,8 +49,8 @@ class SuspiciousActivityReportSeeder extends Seeder
             [
                 'company_id' => $companyId,
                 'client_id' => 13,
-                'reporter_type' => $agentType,
-                'reporter_id' => 1,
+                'reportable_type' => $agentType,
+                'reportable_id' => 1,
                 'anomalies_codes' => json_encode(['4B']),
                 'description' => 'Il nominativo del cliente risulta presente in una lista di soggetti sanzionati internazionali (OFAC). Operazione bloccata in attesa di verifica compliance.',
                 'status' => 'reported',
@@ -59,8 +59,8 @@ class SuspiciousActivityReportSeeder extends Seeder
             [
                 'company_id' => $companyId,
                 'client_id' => 14,
-                'reporter_type' => $employeeType,
-                'reporter_id' => 4,
+                'reportable_type' => $employeeType,
+                'reportable_id' => 4,
                 'anomalies_codes' => json_encode(['1B', '2A']),
                 'description' => 'Struttura operativa complessa con utilizzo di più conti intestati a soggetti collegati per frazionare importi elevati. Sospetto schema di layering.',
                 'status' => 'archived',
