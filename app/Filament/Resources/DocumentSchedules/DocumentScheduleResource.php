a1bf05a6e8ff481bad8bde514c0d0f8f1c6f39b2
Meo-ICAR <piergiuseppe.meo@icar.cnr.it>
2026-06-29 19:13:42 +0200

    rename

--- app/Filament/Resources/DocumentSchedules/DocumentScheduleResource.php
+++ app/Filament/Resources/DocumentSchedules/DocumentScheduleResource.php
@@ -33,7 +33,6 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 // use Illuminate\Support\Collection;
 use App\Models\EmailTemplate;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use pxlrbt\FilamentExcel\Actions\ExportAction;
 use BackedEnum;
